88b0e260f6f16fd0dce47f81d6730cd8ad0b5cee
Juan <jirazusta@codigodelsur.com>
2017-09-23 20:39:22 -0300

    Little fixes

--- app/app.js
+++ app/app.js
@@ -13,30 +13,20 @@ import './styles.scss';
 
 export class App extends React.PureComponent {
   render() {
-    const obj = {
-      game: {
-        name: "Nombre del juego",
-        ranking: [ {nickname: 'Tito', points: 40}, {nickname: 'JP', points: 400},  {nickname: 'Mauri', points: 0}],
-        image: 'http://media.supercheapauto.com.au/sports/images/thumbs/384463-thumb.jpg',
-        description: 'Este juego es de prueba'
-      }
-    }
     return (
       <BrowserRouter history={ history } >
         <div>
           <Header/>
-          <StartMatch match={obj}/>
-          { /* <div className='main-view'>
+          <div className='main-view'>
             <Switch>
               <Route exact path='/' component={ Home }/>
               <Route exact path='/about' component={ About }/>
               <Route exact path='/register' component={ Register }/>
               <Route exact path='/login' component={ Login }/>
               <Route exact path='/match/:match' component={ Match }/>
-              <Route path='/start-match' component={ StartMatch }/>
+              <Route exact path='/start-match' component={ StartMatch }/>
             </Switch>
           </div>
-          */ }
         </div>
         {/*<Footer/>*/}
       </BrowserRouter>
