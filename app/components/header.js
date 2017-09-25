31d65a26c99bb3cf3cd9920f9b5fcf42260b6df4
Guillermo Aguirre <guillermoaguirre1@gmail.com>
2017-09-24 21:09:25 -0300

    Add template to landing

--- app/components/header.js
+++ app/components/header.js
@@ -12,8 +12,40 @@ class Header extends React.PureComponent {
           <Link to='/about'>About</Link>
           <Link to='/login'>Login</Link>
           <Link to='/register'>Register</Link>
-        </div>
       </div>
+      </div>
+      <nav className='navbar navbar-custom navbar-fixed-top' role='navigation'>
+        <div className='container'>
+            <div className='navbar-header'>
+                <button type='button' className='navbar-toggle' data-toggle='collapse' data-target='.navbar-main-collapse'>
+                    Menu <i className='fa fa-bars'></i>
+                </button>
+                <a className='navbar-brand page-scroll' href='#page-top'>
+                  <Link className='logo-img' to='/'><img src={ require('../../assets/images/quizzy_logo.svg') }/></Link>
+                </a>
+            </div>
+
+
+            <div className='collapse navbar-collapse navbar-right navbar-main-collapse'>
+                <ul className='nav navbar-nav'>
+
+                    <li className='hidden'>
+                        <a href='#page-top'></a>
+                    </li>
+                    <li>
+                        <a className='page-scroll' href='#about'>About</a>
+                    </li>
+                    <li>
+                        <a className='page-scroll' href='#download'>Download</a>
+                    </li>
+                    <li>
+                        <a className='page-scroll' href='#contact'>Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+    </nav>
     )
   }
 }
--- app/screens/home.js
+++ app/screens/home.js
@@ -77,10 +77,47 @@ export class Home extends React.Component {
   render() {
     return (
       <div className='main-view'>
-        <div className='table-container'>
-          { this.renderTable() }
-          <Link to={'/all_games'}><button className='button primary long'>ALL GAMES</button></Link>
-        </div>
+
+        <header className="intro">
+            <div className="intro-body">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-8 col-md-offset-2">
+                            <h1 className="brand-heading">Grayscale</h1>
+                            <p className="intro-text">A free, responsive, one page Bootstrap theme.
+                                Created by Start Bootstrap.</p>
+                            <a href="#about" className="btn btn-circle page-scroll">
+                                <i className="fa fa-angle-double-down animated"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+
+        <section id="about" className="container content-section text-center">
+            <div className="row">
+                <div className="col-lg-8 col-lg-offset-2">
+                    <h2>About Grayscale</h2>
+                    <p>Grayscale is a free Bootstrap 3 theme created by Start Bootstrap. It can be yours right now, simply download the template on <a href="http://startbootstrap.com/template-overviews/grayscale/">the preview page</a>. The theme is open source, and you can use it for any purpose, personal or commercial.</p>
+                    <p>This theme features stock photos by <a href="http://gratisography.com/">Gratisography</a> along with a custom Google Maps skin courtesy of <a href="http://snazzymaps.com/">Snazzy Maps</a>.</p>
+                    <p>Grayscale includes full HTML, CSS, and custom JavaScript files along with LESS files for easy customization.</p>
+                </div>
+            </div>
+        </section>
+
+        <section id="download" className="content-section text-center">
+            <div className="download-section">
+                <div className="container">
+                    <div className="col-lg-8 col-lg-offset-2">
+                        <h2>Download Grayscale</h2>
+                        <p>You can download Grayscale for free on the preview page at Start Bootstrap.</p>
+                        <a href="http://startbootstrap.com/template-overviews/grayscale/" className="btn btn-default btn-lg">Visit Download Page</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
         <div className='match-container'>
           <h2>Create or join a match and start playing!</h2>
           <div className='form-container'>
