1b09e3f6737fc700aa778505c872aca27db94adb
Mauricio Irace <thelagger94@gmail.com>
2017-09-29 18:55:43 -0300

    changing to a circular tiemr

--- app/screens/answer-question.js
+++ app/screens/answer-question.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import AnswerButtons from '../components/answer-buttons';
-import { Col, Grid, Jumbotron, Row, Well } from 'react-bootstrap';
-import { Line } from 'rc-progress';
+import { Col, Grid, Jumbotron, PageHeader, Row } from 'react-bootstrap';
 import '../stylesheets/answer-question.scss';
 import { connect } from 'react-redux';
+import Timer from '../components/timer';
 
 const mapDispatchToProps = (dispatch) => {
   return {
@@ -18,19 +18,13 @@ const mapStateToProps = (state,props) => {
 class AnswerQuestion extends React.PureComponent {
   render() {
     return (
-      <div>
-        <Jumbotron>
-          <Grid>
-            <Row>
-              <Col>
-                <h1 className="text-center question"> What is love? </h1>
-                <Line percent='35' strokeWidth='1' strokeColor='#ff8d40' />
-              </Col>
-            </Row>
-          </Grid>
-        </Jumbotron>
-        <Grid fluid>
-          <Row>
+      <Grid fluid>
+        <Row>
+          <Col xsOffset={1} xs={10} >
+            <h1 className="text-center"> What is love? </h1>
+          </Col>
+        </Row>
+        <Row>
             <Col xs={12} mdOffset={3} md={6}>
                 <AnswerButtons/>
             </Col>
@@ -40,12 +34,6 @@ class AnswerQuestion extends React.PureComponent {
             </Col>
           </Row>
         </Grid>
-        <Jumbotron>
-          <h3 className='text-center'> 1/2 </h3>
-        </Jumbotron>
-
-
-      </div>
     )
   }
 }
