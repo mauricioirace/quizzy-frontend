acfd907958ef51dc517d4feed808b84d141ffc7c
Guillermo Aguirre <guillermoaguirre1@gmail.com>
2017-10-02 12:46:57 -0300

    Update home.js

--- app/screens/home.js
+++ app/screens/home.js
@@ -106,99 +106,97 @@ export class Home extends React.Component {
       <div className='main-view' id='page-top'>
         <header className='intro'>
           <div className='intro-body'>
-                <Row>
-                  <Col md={ 7 } mdOffset={ 2 } xs={ 12 }>
-                    <Reveal effect='animated slideInDown'>
-                      <h1 className='brand-heading'>Quizzy</h1>
-                      <h2>Create a game and start playing!</h2>
-                    </Reveal>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col md={ 7 } mdOffset={ 2 } xs={ 12 }>
-                    <div className='match-container'>
-                      <Reveal effect='animated slideInLeft'>
-                        <div className='form-container'>
-                          <div className='form-input horizontal long'>
-                            <label className='fs-22'>quizzy.com/</label>
-                            <input className='fs-16' type='text'
-                             name='game' placeholder='Game name' onChange={ this.handleChange }/>
-                            <Link to={ `/match/${ this.props.matchData.currentMatch }` }
-                              onClick={ this.checkEmptyName } >
-                              <img className='go-button' src={ require('../../assets/images/play_button_fill_shadow.png') }/>
-                            </Link>
-                          </div>
-                          <div className='form-input horizontal medium'>
-                            <span className='error-message'> { this.props.matchData.error ? this.props.matchData.error : null } </span>
-                          </div>
-                        </div>
-                      </Reveal>
+            <Row>
+              <Col md={ 7 } mdOffset={ 2 } xs={ 12 }>
+                <Reveal effect='animated slideInDown'>
+                  <h1 className='brand-heading'>Quizzy</h1>
+                  <h2>Create a game and start playing!</h2>
+                </Reveal>
+              </Col>
+            </Row>
+            <Row>
+              <Col md={ 7 } mdOffset={ 2 } xs={ 12 }>
+                <div className='match-container'>
+                  <Reveal effect='animated slideInLeft'>
+                    <div className='form-container'>
+                      <div className='form-input horizontal long'>
+                        <label className='fs-22'>quizzy.com/</label>
+                        <input className='fs-16' type='text'
+                         name='game' placeholder='Game name' onChange={ this.handleChange }/>
+                        <Link to={ `/match/${ this.props.matchData.currentMatch }` }
+                          onClick={ this.checkEmptyName } >
+                          <img className='go-button' src={ require('../../assets/images/play_button_fill_shadow.png') }/>
+                        </Link>
+                      </div>
+                      <div className='form-input horizontal medium'>
+                        <span className='error-message'> { this.props.matchData.error ? this.props.matchData.error : null } </span>
+                      </div>
                     </div>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col md={ 7 } mdOffset={ 2 } xs={ 12 }>
-                  <Reveal effect='animated slideInUp'>
-                    <a href='#matches' className='btn btn-circle page-scroll'>
-                      <Icon name='angle-double-down' className='animated'></Icon>
-                    </a>
-                    <h2 className='arrow-title'>Or join a live one!</h2>
                   </Reveal>
-                  </Col>
-                </Row>
+                </div>
+              </Col>
+            </Row>
+            <Row>
+              <Col md={ 7 } mdOffset={ 2 } xs={ 12 }>
+              <Reveal effect='animated slideInUp'>
+                <a href='#matches' className='btn btn-circle page-scroll'>
+                  <Icon name='angle-double-down' className='animated'></Icon>
+                </a>
+                <h2 className='arrow-title'>Or join a live one!</h2>
+              </Reveal>
+              </Col>
+            </Row>
           </div>
         </header>
         <div id='matches' className='container content-section text-center'>
-                <Row>
-                  <Col md={ 12 } xs={ 12 }>
-                    <h1 className='brand-heading'>Live games</h1>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col md={ 12 } xs={ 12 }>
-                    <Reveal effect='animated bounceInLeft'>
-                      { this.renderTable() }
-                    </Reveal>
-                  </Col>
-                </Row>
+          <Row>
+            <Col md={ 12 } xs={ 12 }>
+              <h1 className='brand-heading'>Live games</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={ 12 } xs={ 12 }>
+              <Reveal effect='animated bounceInLeft'>
+                { this.renderTable() }
+              </Reveal>
+            </Col>
+          </Row>
         </div>
         <div id='about' className='content-section text-center'>
-            <div className='about-section'>
-                <Row>
-                  <Col md={ 12 } xs={ 12 }>
-                    <h1 className='brand-heading'>How it works</h1>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col md={ 12 } xs={ 12 }>
-                   <Reveal effect='animated fadeIn'>
-                    <h5>Choose a game, answer the questions and prove your knowledge.
-                    There are two modes you can play, normal & real-time.</h5>
-                   </Reveal>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col md={ 12 } xs={ 12 }>
-                    <div className='modes-container'>
-                       <Reveal effect='animated slideInLeft'>
-                        <div className='game-mode'>
-                          <h3>Normal</h3>
-                          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                        </div>
-                       </Reveal>
-                        <Reveal effect='animated slideInRight'>
-                        <div className='game-mode'>
-                          <h3>Real-Time</h3>
-                          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                        </div>
-                       </Reveal>
+          <div className='about-section'>
+            <Row>
+              <Col md={ 12 } xs={ 12 }>
+                <h1 className='brand-heading'>How it works</h1>
+              </Col>
+            </Row>
+            <Row>
+              <Col md={ 12 } xs={ 12 }>
+               <Reveal effect='animated fadeIn'>
+                <h5>Choose a game, answer the questions and prove your knowledge.
+                There are two modes you can play, normal & real-time.</h5>
+               </Reveal>
+              </Col>
+            </Row>
+            <Row>
+              <Col md={ 12 } xs={ 12 }>
+                <div className='modes-container'>
+                   <Reveal effect='animated slideInLeft'>
+                    <div className='game-mode'>
+                      <h3>Normal</h3>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                     </div>
-                  </Col>
-
-                </Row>
-            </div>
+                   </Reveal>
+                    <Reveal effect='animated slideInRight'>
+                    <div className='game-mode'>
+                      <h3>Real-Time</h3>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </div>
+                   </Reveal>
+                </div>
+              </Col>
+            </Row>
+          </div>
         </div>
-
       </div>
     )
   }
