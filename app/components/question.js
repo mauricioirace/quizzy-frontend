844d25c9f6bc8c9e5a701ce50772d1d15637e769
Luis Cuadro <31108175+luisgcuadro@users.noreply.github.com>
2017-10-17 14:30:49 -0300

    spaces

--- app/components/question.js
+++ app/components/question.js
@@ -107,7 +107,7 @@ class Question extends React.PureComponent {
       })
       return;
     } else {
-      this.setState({validText: 'success'})
+      this.setState({ validText: 'success' })
       this.saveChanges();
     }
   }
