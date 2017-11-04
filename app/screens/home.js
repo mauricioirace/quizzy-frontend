5c69187906635baee750dc8ec6e06d1475a551f7
SebasFelix <sebas-felix@hotmail.com>
2017-11-04 02:03:07 -0300

    fixes

--- app/screens/home.js
+++ app/screens/home.js
@@ -91,17 +91,6 @@ export class Home extends React.Component {
     }
   }
 
-  controltag(e) {
-        tecla = (document.all) ? e.keyCode : e.which;
-        console.log(e); 
-        if (tecla==8) return true; // para la tecla de retroseso
-        else if (tecla==0||tecla==9)  return true; //<-- PARA EL TABULADOR-> su keyCode es 9 pero en tecla se esta transformando a 0 asi que porsiacaso los dos
-        patron =/[0-9\s]/;// -> solo letras
-       // patron =/[0-9\s]/;// -> solo numeros
-        te = String.fromCharCode(tecla);
-        return patron.test(te); 
-    }
-
   handleMouseOver() {
     this.isMouseOn = true;
   }
