19afd7ae53a4f49893e3a0073244868c3c132921
Horacio <horaciocolbert@gmail.com>
2017-11-15 20:02:20 -0300

    whole ranking modal added

--- app/screens/end-normal-game.js
+++ app/screens/end-normal-game.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Button, ButtonToolbar, Jumbotron, ListGroup, ListGroupItem, Table, FormGroup, Col,
-        FormControl, Checkbox, Form, ControlLabel } from 'react-bootstrap';
+        FormControl, Checkbox, Form, ControlLabel, Modal } from 'react-bootstrap';
 import { Link, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import '../stylesheets/end-normal-game.scss';
 import { findIndex } from 'underscore';
 import matchService from '../services/match';
+import { Icon } from 'react-fa';
 
 const mapStateToProps = state => {
   return {
@@ -15,6 +16,7 @@ const mapStateToProps = state => {
 }
 
 class EndNormalGame extends React.PureComponent {
+
   constructor(props) {
     super(props);
     this.renderRanking = this.renderRanking.bind(this);
@@ -25,8 +27,12 @@ class EndNormalGame extends React.PureComponent {
     this.player = this.props.match.params.player;
     this.score = this.props.match.params.score;
     this.state = {
-      ranking: []
+      ranking: [],
+      showWhole: false,
     };
+    this.openWhole = this.openWhole.bind(this);
+    this.closeWhole = this.closeWhole.bind(this);
+
   }
 
   componentWillMount() {
@@ -36,7 +42,7 @@ class EndNormalGame extends React.PureComponent {
   componentDidMount() {
     window.scrollTo(0, 0);
   }
-  
+
   getRanking = () => {
     matchService.getRanking(this.id)
     .then((res) => {
@@ -69,7 +75,7 @@ class EndNormalGame extends React.PureComponent {
     )
   }
 
-  renderRanking() {
+  renderRanking(limit) {
     let items = [];
     const lastIndex = this.state.ranking.length - 1;
     if (lastIndex >= 0) {
@@ -80,26 +86,31 @@ class EndNormalGame extends React.PureComponent {
       this.userPosition = userPosition;
       let first;
       let until;
-      if (lastIndex <= 4) {
-        //show until 5 users starting from 0
+      if (limit === -1) {
         first = 0;
         until = lastIndex;
-      } else {
-        //if current user is in first place or second one
-        if (this.userPosition === 0 || this.userPosition === 1) {
-          //show 5 starting from 0
+      }else{
+        if (lastIndex <= 4) {
+          //show until 5 users starting from 0
           first = 0;
-          until = 4;
+          until = lastIndex;
         } else {
-          //if current user is the last or previous than last
-          if (this.userPosition === lastIndex || this.userPosition === lastIndex - 1) {
-            //show 5 starting from lastIndex - 4
-            first = lastIndex - 4;
-            until = lastIndex;
+          //if current user is in first place or second one
+          if (this.userPosition === 0 || this.userPosition === 1) {
+            //show 5 starting from 0
+            first = 0;
+            until = 4;
           } else {
-            //current user is in the middle
-            first = this.userPosition - 2;
-            until = this.userPosition + 2;
+            //if current user is the last or previous than last
+            if (this.userPosition === lastIndex || this.userPosition === lastIndex - 1) {
+              //show 5 starting from lastIndex - 4
+              first = lastIndex - 4;
+              until = lastIndex;
+            } else {
+              //current user is in the middle
+              first = this.userPosition - 2;
+              until = this.userPosition + 2;
+            }
           }
         }
       }
@@ -123,6 +134,14 @@ class EndNormalGame extends React.PureComponent {
     );
   }
 
+  closeWhole() {
+    this.setState({ ...this.state, showWhole: false });
+  }
+
+  openWhole() {
+    this.setState({ ...this.state, showWhole: true });
+  }
+
   render() {
     return (
       <div className='container'>
@@ -130,11 +149,26 @@ class EndNormalGame extends React.PureComponent {
           <h1>Your final score is { this.score }!</h1>
         </Jumbotron>
         <h2>Leaderboard</h2>
+        <button onClick = { this.openWhole } style={ {'float':'right' } } > <i id='expand' className='fa fa-window-maximize' aria-hidden='true'></i> </button>
         <Table responsive>
           { this.renderHeaderRanking() }
-          { this.renderRanking() }
+          { this.renderRanking(4) }
         </Table>
+          <Modal show = { this.state.showWhole } onHide = { this.closeWhole }>
+            <Modal.Header closeButton>
+              <Modal.Title>Leaderboard</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Table responsive>
+                { this.renderHeaderRanking() }
+                { this.renderRanking(-1) }
+              </Table>
+            </Modal.Body>
+          </Modal>
+
       </div>
+
+
     )
   }
 }
